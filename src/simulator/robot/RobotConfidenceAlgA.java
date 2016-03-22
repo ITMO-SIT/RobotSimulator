c50368330cff92f100be49b99e6e3e08b766908f
BLoti <andrew.shlykov@ya.ru>
2016-03-22 22:24:11 +0300

    fix

--- src/simulator/robot/RobotConfidenceAlgA.java
+++ src/simulator/robot/RobotConfidenceAlgA.java
@@ -4,33 +4,29 @@ public class RobotConfidenceAlgA extends AnyLogicRobotConfidence{
 
     @Override
     protected void calcW() {
-        if (neighbors.isEmpty() || robotType == Type.enemy) return;
-        double corrDistX = 0;
-        double corrDistY = 0;
-        int count = 1;
-        for (AnyLogicRobot robot : neighbors) {
-            double distX = robot.getX() - x;
-            double distY = robot.getY() - y;
-            double dist = calcHypotenuse(distX, distY);
-            if (dist > 2 * criticalDist) {
-                distX /= dist;
-                distY /= dist;
-                corrDistX += distX;
-                corrDistY += distY;
-                count++;
+        if (!neighbors.isEmpty() && robotType != Type.enemy) {
+            double normaG1 = calcHypotenuse(gT.getX(), gT.getY());
+            double normaG2 = calcHypotenuse(gF.getX(), gF.getY());
+            double normaH = calcHypotenuse(h.getX(), h.getY());
+            double cosG1H = (h.getX() * gT.getX() + h.getY() * gT.getY()) / normaH / normaG1;
+            double cosG2H = (h.getX() * gF.getX() + h.getY() * gF.getY()) / normaH / normaG2;
+
+            if (cosG1H >= mu) {
+                wT += deltaW;
+                wF -= deltaW;
+                if (wT > 1) wT = 1;
+                if (wF < 0) wF = 0;
+            } else if (cosG2H >= mu) {
+                wT -= deltaW;
+                wF += deltaW;
+                if (wT < 0) wT = 0;
+                if (wF > 1) wF = 1;
+            } else {
+                wT -= deltaW;
+                wF -= deltaW;
+                if (wT < 0) wT = 0;
+                if (wF < 0) wF = 0;
             }
-        }
-        corrDistX /= count;
-        corrDistY /= count;
-
-        double norm = calcHypotenuse(corrDistX, corrDistY);
-
-        if (norm >= mu) {
-            wT += deltaW;
-            if (wT > 1) wT = 1;
-        } else {
-            wT -= deltaW;
-            if (wT < 0) wT = 0;
         }
     }
 }
